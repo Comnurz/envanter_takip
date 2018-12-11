234f717a5ed5e9cd23b7aabd159f26b720925b8c
Kadir <kadirboyraz78@gmail.com>
2018-12-11 16:18:34 +0300

    ana sayfa olusturuldu

--- envanter_takip/urls.py
+++ envanter_takip/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from envanter_takip.views import UrunListView
+from envanter_takip.views import UrunListView, HomePageView
 
 urlpatterns = [
+    path(r'', HomePageView.as_view(), name='ana_sayfa'),
     path(r'urunler', UrunListView.as_view(), name='urun_listesi'),
 ]
--- envanter_takip/views.py
+++ envanter_takip/views.py
@@ -1,24 +1,29 @@
 from django.shortcuts import render
-from envanter_takip.models import Urun
+from envanter_takip.models import Urun, Marka, Kategori, Fatura, Zimmet
 from django.views.generic import View, DetailView, ListView, TemplateView
 
 
 # Create your views here.
+class HomePageView(TemplateView):
+    template_name = 'envanter_takip/index.html'
+    context_object_name = 'urun_listesi'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        urunler = Urun.objects.all()
+        markalar = Marka.objects.all()
+        kategoriler = Kategori.objects.all()
+        faturalar = Fatura.objects.all()
+        context['urunler'] = urunler
+        context['markalar'] = markalar
+        context['kategoriler'] = kategoriler
+        context['faturalar'] = faturalar
+        return context
+
+
 class UrunListView(ListView):
     context_object_name = 'urun_listesi'
     template_name = 'envanter_takip/urunler.html'
 
     def get_queryset(self):
-        urunler = Urun.objects.all()
-        urun_listesi = []
-        for urun in urunler:
-            urun_ = {}
-            urun_['fatura'] = urun.fatura
-            urun_['urun_kod'] = urun.urun_kod
-            urun_['urun_no'] = urun.urun_no
-            urun_['urun_ozellik'] = urun.ozellik
-            urun_['marka'] = urun.marka
-            urun_['kategori'] = urun.kategori
-            urun_['zimmet'] = urun.zimmet
-            urun_listesi.append(urun_)
-        return urun_listesi
+        return Urun.objects.all()
