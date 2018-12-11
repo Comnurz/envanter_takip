b9a2422cab3a1445596ebebe16b755112b9d8a32
Kadir <kadirboyraz78@gmail.com>
2018-12-11 16:22:48 +0300

    kategori sayfasi olusturuldu

--- envanter_takip/urls.py
+++ envanter_takip/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from envanter_takip.views import UrunListView, HomePageView
+from envanter_takip.views import UrunListView, HomePageView, KategoriListView
 
 urlpatterns = [
     path(r'', HomePageView.as_view(), name='ana_sayfa'),
     path(r'urunler', UrunListView.as_view(), name='urun_listesi'),
+    path(r'kategoriler', KategoriListView.as_view(), name='kategoriler'),
 ]
--- envanter_takip/views.py
+++ envanter_takip/views.py
@@ -27,3 +27,10 @@ class UrunListView(ListView):
 
     def get_queryset(self):
         return Urun.objects.all()
+
+class KategoriListView(ListView):
+    context_object_name = 'kategoriler'
+    template_name = 'envanter_takip/kategoriler.html'
+
+    def get_queryset(self):
+        return Kategori.objects.all()
